ce359e51d19049fba502f67c03fd706e9288477f
lucy <lucy@inl>
2017-06-20 22:36:28 +0900

    add image_classification_code

---
+++ image_classification_code/predict.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2017 6/17
+"""
+
+import os
+import pandas as pd
+import pickle
+import numpy as np
+from PIL import Image
+from collections import OrderedDict
+import argparse
+import datetime
+import json
+import multiprocessing
+import random
+import sys
+import threading
+import time
+
+import numpy as np
+from PIL import Image
+
+
+import six
+#import six.moves.cPickle as pickle
+from six.moves import queue
+
+import chainer
+#import matplotlib.pyplot as plt
+import numpy as np
+import math
+import chainer.functions as F
+import chainer.links as L
+from chainer.links import caffe
+from matplotlib.ticker import *
+from chainer import serializers
+from package import nin
+
+
+parser = argparse.ArgumentParser(
+    description='Image inspection using chainer')
+parser.add_argument('--model','-m',default='modelhdf5', help='Path to model file')
+parser.add_argument('--mean', default='mean.npy',
+                    help='Path to the mean file (computed by compute_mean.py)')
+args = parser.parse_args()
+
+PATH_TO_MY_DATA = os.path.join('data','processed')
+PATH_TO_PROCESSED_TEST_IMAGES = os.path.join(PATH_TO_MY_DATA,'processed_test_images')
+PATH_TO_TEST_IMAGES = os.path.join(PATH_TO_MY_DATA, 'test')
+PATH_TO_SUBMIT_FILE = 'submit.csv'
+
+
+def read_image(path, center=False, flip=False):
+  image = np.asarray(Image.open(path)).transpose(2, 0, 1)
+  if center:
+    top = left = cropwidth / 2
+  else:
+    top = random.randint(0, cropwidth - 1)
+    left = random.randint(0, cropwidth - 1)
+  bottom = model.insize + top
+  right = model.insize + left
+  image = image[:, top:bottom, left:right].astype(np.float32)
+  image -= mean_image[:, top:bottom, left:right]
+  image /= 255
+  if flip and random.randint(0, 1) == 0:
+    return image[:, :, ::-1]
+  else:
+    return image
+
+
+mean_image = np.load(os.path.join(PATH_TO_MY_DATA, args.mean))
+
+model = nin.NIN()
+serializers.load_hdf5(os.path.join('models', args.model), model)
+cropwidth = 256 - model.insize
+model.to_cpu()
+
+
+
+def predict(net, x):
+    h = F.max_pooling_2d(F.relu(net.mlpconv1(x)), 3, stride=2)
+    h = F.max_pooling_2d(F.relu(net.mlpconv2(h)), 3, stride=2)
+    h = F.max_pooling_2d(F.relu(net.mlpconv3(h)), 3, stride=2)
+    h = net.mlpconv4(F.dropout(h, train=net.train))
+    h = F.reshape(F.average_pooling_2d(h, 6), (x.data.shape[0], 1000))
+    return F.softmax(h)
+
+
+def create_submit(x, file_name):
+    print('predicting ...')
+    dic = OrderedDict()
+    dic['file_name'] = file_name
+    dic['prediction'] = x
+    print('done.')
+    return pd.DataFrame(dic)
+
+if __name__ == '__main__':
+    X = []
+    files = os.listdir(PATH_TO_PROCESSED_TEST_IMAGES)
+    for f in files: 
+        print(os.path.join(PATH_TO_PROCESSED_TEST_IMAGES, f))
+        img = read_image(os.path.join(PATH_TO_PROCESSED_TEST_IMAGES, f))
+        x = np.ndarray(
+            (1, 3, model.insize, model.insize), dtype=np.float32)
+        x[0]=img
+        x = chainer.Variable(np.asarray(x), volatile='on')
+
+        score = predict(model, x)
+        index = np.argmax(score.data[0].tolist())
+        X.append(index)
+        #categories = np.loadtxt("labels.txt", str, delimiter='\t')
+        categories = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+        top_k = 5
+        prediction = list(zip(score.data[0].tolist(), categories))
+        prediction.sort(key = lambda x: x, reverse=True)
+        for rank, (score, color) in enumerate(prediction[:top_k], start=1):
+            print('#%d | %d color | %4.1f%%' % (rank, color, score * 100))
+        print("1 rank index is: ", index)
+    submit = create_submit(X, files)
+    submit.to_csv(PATH_TO_SUBMIT_FILE, index=None, header=None)
---
+++ image_classification_code/preprocess.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2017 6/17
+"""
+
+import os
+import zipfile
+import cv2
+from PIL import Image
+from package.preprocess_methods import my_preprocess_method
+import argparse
+import sys
+
+import numpy as np
+import chainer
+
+PATH_TO_GIVEN_DATA = os.path.join('data','given')
+PATH_TO_MY_DATA = os.path.join('data','processed')
+if not os.path.exists(PATH_TO_MY_DATA):
+    os.mkdir(PATH_TO_MY_DATA)
+
+PATH_TO_TRAIN_IMAGES = os.path.join(PATH_TO_MY_DATA, 'train')
+PATH_TO_TEST_IMAGES = os.path.join(PATH_TO_MY_DATA, 'test')
+
+PATH_TO_DST_TRAIN_IMAGES = os.path.join(PATH_TO_MY_DATA, 'processed_train_images')
+PATH_TO_DST_TEST_IMAGES = os.path.join(PATH_TO_MY_DATA, 'processed_test_images')
+
+if not os.path.exists(PATH_TO_DST_TRAIN_IMAGES):
+    os.mkdir(PATH_TO_DST_TRAIN_IMAGES)
+if not os.path.exists(PATH_TO_DST_TEST_IMAGES):
+    os.mkdir(PATH_TO_DST_TEST_IMAGES)
+
+def extract_zipfile(src, dst):
+    print('extracting '+src+' to '+dst+' ...')
+    with zipfile.ZipFile(src, 'r') as zip_file:
+        zip_file.extractall(dst)
+    print('done.')
+
+def preprocess_image(path_to_images, path_to_dst_images):
+    target_shape = (256, 256)
+    output_side_length = 256
+    files = os.listdir(path_to_images)
+    for f in files:
+        print(path_to_images + '/' + f)
+        img = cv2.imread(path_to_images + '/' + f)
+        height, width, depth = img.shape
+        new_height = output_side_length
+        new_width = output_side_length
+
+        if height > width:
+            new_height = int(output_side_length * height / width)
+        else:
+            new_width = int(output_side_length * width / height)
+
+        resized_img = cv2.resize(img, (new_width, new_height))
+        height_offset = int((new_height - output_side_length) / 2)
+        width_offset = int((new_width - output_side_length) / 2)
+        cropped_img = resized_img[height_offset:height_offset + output_side_length,width_offset:width_offset + output_side_length]
+        cv2.imwrite(path_to_dst_images + '/' + f, cropped_img)
+
+
+def compute_mean(dataset):
+    print('compute mean image')
+    sum_image = 0
+    N = len(dataset)
+    for i, (image, _) in enumerate(dataset):
+        sum_image += image
+        sys.stderr.write('{} / {}\r'.format(i, N))
+        sys.stderr.flush()
+    sys.stderr.write('\n')
+    return sum_image / N
+
+
+
+if __name__ == '__main__':
+    ## extract the given zipfiles(or maybe tarfiles)
+
+    if not os.path.exists(PATH_TO_TRAIN_IMAGES):
+        extract_zipfile(os.path.join(PATH_TO_GIVEN_DATA, 'train_images.zip'), PATH_TO_MY_DATA)
+        preprocess_image(PATH_TO_TRAIN_IMAGES, PATH_TO_DST_TRAIN_IMAGES)
+    if not os.path.exists(PATH_TO_TEST_IMAGES):
+        extract_zipfile(os.path.join(PATH_TO_GIVEN_DATA, 'test_images.zip'), PATH_TO_MY_DATA)
+        preprocess_image(PATH_TO_TEST_IMAGES, PATH_TO_DST_TEST_IMAGES)
+
+
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    fp = open(PATH_TO_MY_DATA + '/train_master.txt', 'w')
+
+    with open(PATH_TO_GIVEN_DATA + '/train_master.tsv','r') as f:
+        for row in f:
+            train = row[:-1].split('\t')
+            if train[1] == 'category_id': 
+                print('skip header')
+                continue
+            fp.write(current_path + "/" + PATH_TO_DST_TRAIN_IMAGES + "/" + train[0] + " " + train[1] + "\n")
+
+    fp.close() 
+
+    dataset = chainer.datasets.LabeledImageDataset(PATH_TO_MY_DATA + "/train_master.txt", ".")
+    mean = compute_mean(dataset)
+    np.save(PATH_TO_MY_DATA + "/mean.npy", mean)
+
